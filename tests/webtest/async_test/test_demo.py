35ccf91b2e6bdb385725502c23598436d017e69c
yxma2010 <yxma2010@shuhe.com>
2025-07-28 10:43:14 +0800

    移除 tests/webtest 目录的 Git 跟踪

--- tests/webtest/async_test/test_demo.py
+++
@@ -1,8 +0,0 @@
-from playwright.async_api import Page, expect
-import pytest, asyncio
-@pytest.mark.asyncio
-async def test_async_demo(authed_page:Page):
-    print("event_loop:", asyncio.get_event_loop())
-    await authed_page.goto("http://moka.dmz.sit.caijj.net/bizui/#/activity-list/v2/liufang_test?planTabKey=plan")
-    await expect(authed_page).to_have_title("计划配置")
-    await authed_page.close()
